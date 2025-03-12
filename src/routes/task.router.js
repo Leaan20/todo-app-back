bf1a46b671e9a640bd8728c5618138cfd810112c
Leaan20 <leandro.ariel.fernandez20@gmail.com>
2025-03-12 11:43:30 -0300

    agregamos un controller a nuestra app

--- src/routes/task.router.js
+++ src/routes/task.router.js
@@ -1,96 +1,20 @@
 import { Router } from 'express';
-import { taskModel } from '../models/task.model.js';
+// importamos nuestro controller donde se halla la logica de nuestro CRUD
+import { allTasks, findTask, createTask, updateTask, deleteTask } from '../controller/task.controller.js';
 
+// utilizar un controller , nos permite un codigo mas facil de mantener, ademas de mejor y mas facil lectura
 
 const router = Router();
 
-router.get('/', async  (req,res) => {
-    const allTasks = await taskModel.find();
-    try {
+router.get('/', allTasks );
 
-        if(!allTasks) return res.status(404).send({message: 'No hay tasks para mostrar'});
+router.get('/:tid', findTask );
 
-        return res.status(200).send({tasks: allTasks, message: 'tasks obtenidas exitosamente' });
-    } catch (error) {
-        console.log(error);
-        
-        res.status(500).send({status: 'error', message: 'Hubo un error al obtener las tasks'})
-    }
-});
+router.post('/', createTask );
 
-router.get('/:tid', async (req,res) => {
-    const { tid } = req.params;
-    try {
-        if(!tid) return 'Es necesario el id del task';
+router.patch('/:tid', updateTask );
 
-        const task = await taskModel.findById(tid);
-
-        res.status(200).send({message: 'task encontrada', task: task});
-    } catch (error) {
-        console.log(error);
-        
-        res.status(500).send({error, message:'error al encontrar la task indicada'})
-    }
-})
-
-router.post('/', async (req,res) => {
-    const { title, completed } = req.body;
-
-    try {
-        if(!title || title.trim() === '') return 'No puede guardarse una task vacia';
-
-        const newTask = await taskModel.create({
-            title,
-            completed
-        });
-        res.status(201).send({message:'nueva task creada exitosamente', task: newTask});
-    } catch (error) {
-        console.log(error);
-        res.status(501).send({error,message: 'Error al crear una nueva task'});
-    }
-});
-
-
-
-router.patch('/:tid', async (req,res) => {
-        const { tid } = req.params;
-        const { completed, title } = req.body;
-        
-    try {
-        if(!tid) return 'Es necesario el id del task';
-        
-        const updateFields = {};
-        if (completed !== undefined) updateFields.completed = completed;
-        if (title !== undefined && title.trim() !== '') updateFields.title = title;
-
-        const taskUpdated = await taskModel.findByIdAndUpdate(
-            tid,
-            updateFields,
-            { new: true }
-        );
-
-        res.status(203).send({message: 'task modificada con exito', taskUpdated});
-        
-    } catch (error) {
-        console.log(error);
-        res.status(501).send({error, message: 'No pudo actualizarse el estado de la task'});
-    }
-});
-
-router.delete('/:tid', async (req,res) => {
-        const { tid } = req.params;
-        try {
-            if(!tid) return 'Es necesario el id del task para eliminarlo';
-
-        const taskDeleted = await taskModel.findByIdAndDelete(tid);
-
-        res.status(200).send({message: 'Task eliminada de forma exitosa', taskDeleted})
-        } catch (error) {
-            console.log(error);
-            res.status(500).send({error,message:'Error al intentar eliminar la task'})
-            
-        }
-});
+router.delete('/:tid', deleteTask );
 
 
 
